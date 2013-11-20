4eab7237619859a070b6e6c65895dffff6076488
JakeCataford <JakeCataford@Gmail.com>
2013-11-20 14:18:03 -0500

    Adds copy and proper slides...

--- js/sloe-components.js
+++ js/sloe-components.js
@@ -1,5 +1,4 @@
 (function() {
-  console.log("wut");
   window.SLOE = {
     components :[],
 
--- js/sloe.js
+++ js/sloe.js
@@ -9,17 +9,57 @@ $(document).ready(function() {
     },
     duration : 1500
   });
+
   SLOE.make.slide({
     context : {
-      "title" : "Main Street renewal",
+      "title" : "Old Ottawa East (OOE) is…",
+      "body" : "A “hidden gem” of a community, located between the Rideau Canal and the Rideau \
+                River in central Ottawa",
+      backgroundImage : "assets/img/trees-and-path.png",
+      backgroundParallaxDistance : 500
+    },
+    
+    duration : 2500
+  });
+
+  SLOE.make.slide({
+    context : {
+      "title" : "OOE is changing…",
+      "body" : "<p>Development is increasing due to the desirable location of OOE.<br/><br/>\
+                At the same time, OOE community groups are working to make OOE more\
+                environmentally sustainable with a better quality of life</p>",
+      backgroundImage : "assets/img/trees-and-path.png",
+      backgroundParallaxDistance : 500
+    },
+    
+    duration : 2500
+  });
+
+  SLOE.make.timeline({
+    context : {
+      "title" : "What's Been Achieved in Recent Years?",
+      backgroundColor : "#e37482",
+      stripeColor : "#237272",
+      iconImage : "assets/img/test-icon.png",
+      backgroundParallaxDistance : 500
+    },
+    
+    duration : 2500
+  });
+  
+
+  SLOE.make.colorSlide({
+    context : {
+      "title" : "Farmers Market",
       "body" : "\
         <ul>\
-          <li>Goal is to engage children with nature.</li>\
-          <li>Anyone is welcome to help out in the garden, and to harvest from the garden.</li>\
-          <li>Hosts play-groups, art-making workshops, organic gardening workshops, harvest feasts and more.</li>\
+          <li>Is the only community-run Farmers Market in Ontario.</li>\
+          <li>Local food = smaller environmental footprint and stronger local economy</li>\
+          <li>A fun and vibrant meeting place that builds community.</li>\
         </ul>\
       ",
-      backgroundImage : "assets/img/trees-and-path.png",
+      backgroundColor : "#eeaede",
+      iconImage : "assets/img/test-icon.png",
       backgroundParallaxDistance : 500
     },
     
@@ -28,7 +68,7 @@ $(document).ready(function() {
 
   SLOE.make.colorSlide({
     context : {
-      "title" : "Main Street renewal",
+      "title" : "Children’s Garden",
       "body" : "\
         <ul>\
           <li>Goal is to engage children with nature.</li>\
@@ -44,17 +84,19 @@ $(document).ready(function() {
     duration : 2500
   });
 
-  SLOE.make.slide({
+  SLOE.make.colorSlide({
     context : {
-      "title" : "Main Street renewal",
+      "title" : "Community Gardens",
       "body" : "\
         <ul>\
-          <li>Goal is to engage children with nature.</li>\
-          <li>Anyone is welcome to help out in the garden, and to harvest from the garden.</li>\
-          <li>Hosts play-groups, art-making workshops, organic gardening workshops, harvest feasts and more.</li>\
+          <li>Allows residents without suitable space to grow their own vegetables on community land</li>\
+          <li>Two locations: <br/>\
+              - Ottawa East Community Garden on St Paul’s/Oblates land <br/>\
+              - Lees Community Garden next to apartment buildings</li>\
         </ul>\
       ",
-      backgroundImage : "assets/img/trees-and-path.png",
+      backgroundColor : "#345622",
+      iconImage : "assets/img/test-icon.png",
       backgroundParallaxDistance : 500
     },
     
@@ -63,12 +105,105 @@ $(document).ready(function() {
 
   SLOE.make.colorSlide({
     context : {
-      "title" : "Main Street renewal",
+      "title" : "Brantwood Park Field House Renovation",
+      "body" : "<ul>\
+          <li>Field house is now a multi-use space for all ages.</li>\
+          <li>Activities include playgroups, art classes, aftercare, sports and games, as well as changing areas for skating and swimming.</li>\
+        </ul>\
+      ",
+      backgroundColor : "#345622",
+      iconImage : "assets/img/test-icon.png",
+      backgroundParallaxDistance : 500
+    },
+    
+    duration : 2500
+  });
+
+  SLOE.make.slide({
+    context : {
+      "title" : "Community Design Plan",
+      "body" : "<ul>\
+                <li>Community participated in creation of the community design plan.</li>\
+                <li>Activities include playgroups, art classes, aftercare, sports and games, as well as changing areas for skating and swimming.<li>\
+                </ul>",
+    backgroundImage : "assets/img/trees-and-path.png",
+    backgroundParallaxDistance : 500
+  },
+    duration : 2500
+  });
+
+  SLOE.make.timeline({
+    context : {
+      "title" : "What Initiatives Are In Progress?",
+      backgroundColor : "#e37482",
+      stripeColor : "#237272",
+      iconImage : "assets/img/test-icon.png",
+      backgroundParallaxDistance : 500
+    },
+    
+    duration : 2500
+  });
+
+  SLOE.make.colorSlide({
+    context : {
+      "title" : "Rideau River Nature Trail",
       "body" : "\
         <ul>\
-          <li>Goal is to engage children with nature.</li>\
-          <li>Anyone is welcome to help out in the garden, and to harvest from the garden.</li>\
-          <li>Hosts play-groups, art-making workshops, organic gardening workshops, harvest feasts and more.</li>\
+          <li>The community installed interpretative panels, worked with City staff on the Springhurst \
+              Park path design and the dock at Clegg, improved the health and diversity of shoreline \
+              vegetation, developed a plan for the Brantwood Park trail and installed amenities such \
+              as the sitting rocks</li> \
+          <li>Community is providing extensive input on City’s “Western Rideau” pathway design</li>\
+        </ul>",
+      backgroundColor : "#eeaede",
+      iconImage : "assets/img/test-icon.png",
+      backgroundParallaxDistance : 500
+    },
+    
+    duration : 2500
+  });
+
+  SLOE.make.colorSlide({
+    context : {
+      "title" : "Main Street Renewal",
+      "body" : "\
+        <ul>\
+          <li>Main Street is slated for reconstruction in 2014-15</li>\
+          <li>The community pushed for a “Complete Street” approach including bike lanes, wide sidewalks, and slower traffic, and the plan was approved by City</li>\
+          <li>The end result will be an attractive, safe and vibrant Main Street that can more easily support shopping and other amenities</li>\
+        </ul>\
+      ",
+      backgroundColor : "#eeaede",
+      iconImage : "assets/img/test-icon.png",
+      backgroundParallaxDistance : 500
+    },
+    
+    duration : 2500
+  });
+
+  SLOE.make.colorSlide({
+    context : {
+      "title" : "Canal Footbridge at Clegg",
+      "body" : "\
+        <ul>\
+          <li>The community advocated for the footbridge in order to improve access to amenities and reduce car use</li>\
+          <li>Design and environmental assessment has been approved by City, and is waiting for funding</li>\
+        </ul>\
+      ",
+      backgroundColor : "#345622",
+      iconImage : "assets/img/test-icon.png",
+      backgroundParallaxDistance : 500
+    },
+    
+    duration : 2500
+  });
+
+  SLOE.make.colorSlide({
+    context : {
+      "title" : "Brantwood Park Field House Renovation",
+      "body" : "<ul>\
+          <li>Field house is now a multi-use space for all ages.</li>\
+          <li>Activities include playgroups, art classes, aftercare, sports and games, as well as changing areas for skating and swimming.</li>\
         </ul>\
       ",
       backgroundColor : "#345622",
@@ -81,16 +216,81 @@ $(document).ready(function() {
 
   SLOE.make.timeline({
     context : {
-      "title" : "Main Street renewal",
-      "subtitle" : "whut.",
+      "title" : "What’s Next for OOE?",
       backgroundColor : "#e37482",
-      stripeColor : "#283992",
+      stripeColor : "#237272",
       iconImage : "assets/img/test-icon.png",
       backgroundParallaxDistance : 500
     },
     
     duration : 2500
   });
+
+  SLOE.make.slide({
+    context : {
+      "title" : "OOE Population Is Growing...",
+      "body" : "<ul>\
+                <li>The population in OOE is expected to double within 20 years as a result of:</li>\
+                <li>- Intensification from infill and development on Main Street and Hawthorne.</li>\
+                <li>- Development of institutional lands near Saint Paul’s University (land owned by Oblates and Sisters) – additional 3000 residents</li>\
+                <li>- Transit-Oriented Development around Lees Transit Station – could ultimately add 20,000 residents</li>\
+                </ul>",
+    backgroundImage : "assets/img/trees-and-path.png",
+    backgroundParallaxDistance : 500
+  },
+    duration : 2500
+  });
+
+  SLOE.make.slide({
+    context : {
+      "title" : "How Can OOE Benefit?",
+      "body" : "<ul>\
+                <li>Well planned new development can improve quality of life and sustainability in Old Ottawa East community through:</li>\
+                <li>- Increased community services and amenities to meet community needs<li>\
+                <li>- A wider mix of housing types that is more socially inclusive<li>\
+                <li>- Improved connectivity within the community so it is easier to walk and cycle<li>\
+                </ul>",
+      backgroundImage : "assets/img/title-slide-bg.png",
+      backgroundParallaxDistance : 500
+    },
+    duration : 2500
+  });
+
+  SLOE.make.slide({
+    context : {
+      "title" : "Sustainability Goals for Oblate Land Development",
+      "body" : "<ul>\
+                <li>SLOE is working on researching and promoting sustainable development options for the Oblate lands in the following areas:</li>\
+                <li>- Energy sustainability through district energy systems<li>\
+                <li>- Development of seniors and affordable housing<li>\
+                <li>- Improved connectivity and community amenities/services<li>\
+                <li>- Stormwater management on site and shoreline restoration<li>\
+                </ul>",
+      backgroundImage : "assets/img/title-slide-bg.png",
+      backgroundParallaxDistance : 500
+    },
+    duration : 2500
+  });
+
+  SLOE.make.colorSlide({
+    context : {
+      "title" : "Get Involved in the Future of OOE",
+      "body" : "\
+        <ul>\
+          <li>To realize the potential of the OOE community, we need you!</li>\
+          <li>Help make OOE a resilient, sustainable community that is the ideal place to live, play and study.</li>\
+        </ul>\
+      ",
+      backgroundColor : "#eeaede",
+      iconImage : "assets/img/test-icon.png",
+      backgroundParallaxDistance : 500
+    },
+    
+    duration : 2500
+  });
+
+  
+  
 
   skrollr.init();
 });
